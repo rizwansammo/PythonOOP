4ab1ea1e44a2321ee7a8dbcb4ffe9f49f24d14e7
ZeroByte <mrizwan.sammo@gmail.com>
2022-11-08 14:56:28 +0600

    OOP 2.8

---
+++ box.py
@@ -0,0 +1,34 @@
+# 8
+class box:
+    def __init__(self, measure):
+        print('Creating a box!')
+        vol = measure[0] * measure[1] * measure[2]
+        print(f"Volume of the box is {vol} cubic units")
+        self.height = measure[0]
+        self.width = measure[1]
+        self.breadth = measure[2]
+
+print("Box 1")
+b1 = box([10,10,10])
+print("=========================")
+print("Height:", b1.height)
+print("Width:", b1.width)
+print("Breadth:", b1.breadth)
+print("-------------------------")
+print("Box 2")
+b2 = box((30,10,10))
+print("=========================")
+print("Height:", b2.height)
+print("Width:", b2.width)
+print("Breadth:", b2.breadth)
+b2.height = 300
+print("Updating Box 2!")
+print("Height:", b2.height)
+print("Width:", b2.width)
+print("Breadth:", b2.breadth)
+print("-------------------------")
+print("Box 3")
+b3 = b2
+print("Height:", b3.height)
+print("Width:", b3.width)
+print("Breadth:", b3.breadth)
