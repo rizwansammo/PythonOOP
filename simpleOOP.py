d1d50a1370ab32e43d993367f1bc8e7f3fbd379f
ZeroByte <mrizwan.sammo@gmail.com>
2022-11-08 14:42:58 +0600

    Simple Python OOP

---
+++ simpleOOP.py
@@ -0,0 +1,18 @@
+# 1
+class DataType:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+#Write your class code here
+data_type1 = DataType('Integer', 1234)
+print(data_type1.name)
+print(data_type1.value)
+print('=====================')
+data_type2 = DataType('String', 'Hello')
+print(data_type2.name)
+print(data_type2.value)
+print('=====================')
+data_type3 = DataType('Float', 4.0)
+print(data_type3.name)
+print(data_type3.value)
